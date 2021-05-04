c293f5daf22a4277ab95445eaff89d09c885e954
sirget <37872290+sirget@users.noreply.github.com>
2021-05-04 09:54:14 +0700

    install dot net

---
+++ Software-Studio-Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Software_Studio_Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
