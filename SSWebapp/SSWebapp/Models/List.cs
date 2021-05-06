08c89e75f418a5938a695b747700df01a5a47d25
sirget <37872290+sirget@users.noreply.github.com>
2021-05-06 20:44:07 +0700

    upload

---
+++ SSWebapp/SSWebapp/Models/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SSWebapp.Models
+{
+    public class List
+    {
+        public string Id { get; set; }
+        public int Lab { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Bookdate { get; set; }
+
+    }
+}
