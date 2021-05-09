fab5316a8774bfce37ece95fe4c79babbbe2f05a
benzjakk <benzsport357@gmail.com>
2021-05-09 16:59:10 +0700

    update Select Booking Time Page
    Prototype 1

--- frontend/time-card.js
+++ frontend/time-card.js
@@ -3,26 +3,57 @@ template.innerHTML=`
 <style>
 .hourcard {
     width: 100px;
-    height: 100px;
-    background-color: #59fd3e;
-    padding: 1px;
+    height: 126px;
+    background-color: #00BFA5;
     margin: 5px;
+    border-radius: 20px;
+    display:flex;
+    flex-direction:column;
+    align-items: center;
   }
-.hourcard p {
+.hourcard .time{
+    border-radius: 20px;
+    width: 100px;
+    height: 100px;
+    background-color: #009688;
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    justify-content: center;
+}
+.hourcard .hour {
     font-size: medium;
+    color:white;
+    margin:0;
   }
+
+.hourcard .vol{
+    color:white;
+    font-size:10px;
+}
+.hourcard .start{
+    font-size:24px;
+    font-weight:bold;
+}
 </style>
 <div class="hourcard">
-            <p id="hour">8.00</p>
-            <p id="vol">20</p>
-          </div>`
+    <div class="time">
+        <p class="hour start">8:00</p>
+        <p class="hour">-9:00</p>
+    </div>
+        <p class="vol">remain: 10</p>
+</div>`
 class TimeCard extends HTMLElement{
     constructor(){
         super();
         this.attachShadow({mode:'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.getElementById("hour").innerText=this.getAttribute('hour');
-        this.shadowRoot.getElementById("vol").innerText=this.getAttribute('vol');
+        //this.shadowRoot.getElementById("hour").innerText=this.getAttribute('hour');
+        //this.shadowRoot.getElementById("vol").innerText=this.getAttribute('vol');
+        if(this.getAttribute('enough')=="true"){
+            console.log('red');
+            
+        }
         
 
         
