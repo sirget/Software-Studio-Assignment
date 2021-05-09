f70717fdcebe5b4e8ab8b1415e307ca1b4184a9c
benzjakk <benzsport357@gmail.com>
2021-05-09 15:18:08 +0700

    update lab page

--- frontend/lab-card.js
+++ frontend/lab-card.js
@@ -81,7 +81,7 @@ p{
     <div class="detail">
     <h3>Voltmeter</h3>
     <label>12 pcs</label>
-    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    <p></p>
     <button>BOOK</button>
     </div>
   </div>`
@@ -90,8 +90,11 @@ class LabCard extends HTMLElement{
         super();
         this.attachShadow({mode:'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.getElementById("cardhead").innerText=this.getAttribute('name');
+        this.shadowRoot.getElementById("cardhead").innerText=this.getAttribute('lab');
         this.shadowRoot.querySelector('img').setAttribute('src',this.getAttribute('image'));
+        this.shadowRoot.querySelector('p').innerText=this.getAttribute('desc');
+        this.shadowRoot.querySelector('label').innerText=this.getAttribute('vol')+" pieces";
+        this.shadowRoot.querySelector('h3').innerText=this.getAttribute('itemName');
 
 
         
