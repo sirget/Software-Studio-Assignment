2d1ecffed4ef800fba371dcfa641243a023ad39e
Tanabodee Sangprajong <deempooh@gmail.com>
2021-05-22 15:16:40 +0700

    test

--- MVC-Test/Controllers/HelloWorldController.cs
+++ MVC-Test/Controllers/HelloWorldController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MVC_Test.Controllers
 {
     public class HelloWorldController : Controller
     {
--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,11 +8,10 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
-
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
         public string Welcome(string name, int ID = 1)
